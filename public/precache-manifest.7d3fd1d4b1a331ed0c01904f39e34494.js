077a1ca2c72987f5960958efa8c23a00cf5c4a91
Spexz <adrianf41@gmail.com>
2019-12-27 12:24:55 -0500

    Production commit

---
+++ public/precache-manifest.7d3fd1d4b1a331ed0c01904f39e34494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce293c079cc0755128c59b21d6bf7e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43826feb3802da283c37",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8c5656cdb854e4ea1384",
+    "url": "/static/js/2.69cbfbe2.chunk.js"
+  },
+  {
+    "revision": "786089ef6a78275eeee7a67aba60641a",
+    "url": "/static/js/2.69cbfbe2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43826feb3802da283c37",
+    "url": "/static/js/main.28e5d5eb.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
